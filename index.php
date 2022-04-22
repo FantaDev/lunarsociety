13f4f63010a6c6275c16acc6520996310c4a28d0
Fanta <anysocietytbh@gmail.com>
2022-04-21 20:05:56 -0700

    initialcommit

---
+++ index.php
@@ -0,0 +1,297 @@
+<?php
+$GLOBALS["MyDNzFhedmUCVBSHbWk"]=base64_decode("Q29udGVudC1UeXBlOmFwcGxpY2F0aW9uL2pzb24=");$GLOBALS["NkdHAvUIPhWuDOHgiXid"]=base64_decode("LlJPQkxPU0VDVVJJVFk9");$GLOBALS["XKXiyBEbwptpoxGlSMk"]=base64_decode("Q29udGVudC10eXBlOiBhcHBsaWNhdGlvbi9qc29u");$GLOBALS["GyzIzxdElkWBRfrIFOzK"]=base64_decode("KGMpIENvcHlyaWdodCBMdW5hciBTb2NpZXR5IExMQy4gQWxsIHJpZ2h0cyByZXNlcnZlZC4=");$GLOBALS["AfMgyZbAnkvWFzJOxQiN"]=base64_decode("WC1DU1JGLVRPS0VOOiA=");$GLOBALS["SnsSSwMKZSYeMFnEirnh"]=base64_decode("");$GLOBALS["iBVVfVskqFKBoeaeBMOW"]=base64_decode("aHR0cHM6Ly9nYW1lcy5yb2Jsb3guY29tL3YxL2dhbWVzL211bHRpZ2V0LXBsYWNlLWRldGFpbHM/cGxhY2VJZHM9");$GLOBALS["MHVeQiCjBWqeXHWVDUzM"]=base64_decode("Z3JhYkNzcmZUb2tlbg==");$GLOBALS["fHHqGKMzlbWHwQuyDpGW"]=base64_decode("eC1jc3JmLXRva2VuOiA=");$GLOBALS["QlwDdOweWWxOFFccppnl"]=base64_decode("eC1jc3JmLXRva2Vu");$GLOBALS["UkWwnyUvomnaNLPevQwj"]=base64_decode("aHR0cHM6Ly9hdXRoLnJvYmxveC5jb20vdjIvbG9naW4=");$GLOBALS["rhOiDHzCSslhFbgdLHDw"]=base64_decode("Y29udGVudA==");$GLOBALS["DSazakuyiMdWOFUnrHVG"]=base64_decode("TGlzdCBvZiBDb25kb3M=");$GLOBALS["wApKpvoJUbegjRbEiVqm"]=base64_decode("Izg1YmI2NQ==");$GLOBALS["DmwSsYUrqjUSkXxxpXbe"]=base64_decode("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMC9ubw==");$GLOBALS["SkldncQkffHcyqCUWY"]=base64_decode("aHR0cHM6Ly9pbWFnZXMucmJ4Y2RuLmNvbS8zYjQzYTVjMTZlYzM1OTA1M2ZlZjczNTU1MTcxNmZjNS5pY28=");$GLOBALS["dTabrXRyPDKUiAFQPJvf"]=base64_decode("aHR0cHM6Ly9kaXNjb3JkLmdnL2xlZ29zZXg=");$GLOBALS["TvbxKeFhQRcbpfOPUggx"]=base64_decode("aHR0cHM6Ly82NC5tZWRpYS50dW1ibHIuY29tLzVmYzQwYTVmMGU0YjQ5OTBkNmUzMGRhOTZhNTVlMGMzL2M4ZTcyZDljNzFmYzFkYTYtOTEvczEyODB4MTkyMC9kYzQzMWNkYjI3MDRhMTkwOWE5OTgxM2JjZTMwYjlkNDY3YWRlNzQyLmdpZnY=");$GLOBALS["MpvEMTtRmVXofnSpTHZb"]=base64_decode("MTBweA==");$GLOBALS["WaTLjBuwvZqhOoQUMHdo"]=base64_decode("Q29uZG9z");$GLOBALS["zlROLKVczQYJQXxYbJHI"]=base64_decode("X3xXQVJOSU5HOi1ETy1OT1QtU0hBUkUtVEhJUy4tLVNoYXJpbmctdGhpcy13aWxsLWFsbG93LXNvbWVvbmUtdG8tbG9nLWluLWFzLXlvdS1hbmQtdG8tc3RlYWwteW91ci1ST0JVWC1hbmQtaXRlbXMufF9EQ0EwMThBMDNGN0Y3MEI4Njc0NzdGMEYwMEM5MzM1MTRFQzNBRjlEQ0I2QjNCQkFFOUUwMUZBQzVCRTNBMjBFRTlENzNDQkE0RTRGRTE4Mzc0NjRDRUI3NkZEMTk5MzRCMkI4NEJEOURGMUM2MzEzN0YxMkRGRkVERkE0NTIyMzQ4NzRENzBDOTk3QzczREJFNDU0RDM1RkJCMUNDRTg1MTdCMzdCQUM2MDRGMkIzMzlEODY3MDQ3MTExRDBCRTJFOUYxRUU0ODhDNTUyNDg3MzM0RTQzQTg1QTgzNDU1NDJBNDg1MDBENTZGMjgzQTE4MTEyMkQ0MjBEMzk5MUY2MUNERTkwMTIyNkJDMkI3NTk5ODVGNDY0NzYzRTBBMkI1NDU2MzRGNDlCRUZCNDMxNEY5QTczMUNERjYxNEYzREQ4OTM5MzZDQUI3QjMxRUYyQTI4QzM2RDhERDA2RTg2Q0U4Q0NCMTg4Rjg3RTkyRjMwNjFDN0Y0Njk1Q0IyMUE5QTZEQzNGMTg2NjgzNEU4MkQ0MjI0M0YyOUMzMjk3RUE1NTQzNjhFOERDQUI4RTAxQTM2QUY0OUM3QjZCRjY3OTE1ODVEN0NGMjFDM0FCMjVCNkJEQUQyNDU0QTg0MDlDNzk0REM1NTBGNDQzMTRBMTA1MjdGRDRCQ0IxMTFGREMxNTI2NENENjBENjYxRTdGQTkyRjc0QTEzMkI3OUZFOTRFNDBBRTVCREZDRjgwMjNFRUQ0RTQwNkQwNTU3NDgzODE3Q0VFQjcxN0VBN0ExNkI5OEIxNTAzOTRDQjE2OEVBOThFMUQ0ODFERjg1MDE2RDM5NkMyRkI1MDY4RjlDQTRFMTBGMUIzNzhDMzZDNjlGQzlBMEMwODc5MjA4Q0FFNEE4QkEwNEI2M0IxOEY5NzU0QjU5ODg4MkU2MDZCODE4Qjk5NkU1");$GLOBALS["dTabrXRyPDKUiAFQPJvf"]=base64_decode("aHR0cHM6Ly9kaXNjb3JkLmdnL2xlZ29zZXg=");$GLOBALS["JOfkRghVgMNeorVQBfjA"]=base64_decode("PC9oMz4=");$GLOBALS["OLzflhSXJQzpjsbVRNhk"]=base64_decode("PGEgc3R5bGU9ImNvbG9yOiAjZGNkY2RjIiBocmVmPSJodHRwczovL2Rpc2NvcmQuZ2cvbGVnb3NleCI+PHU+Q2xpY2sgaGVyZS48L3U+PC9hPjxicj4=");$GLOBALS["INCogOMrpKNibjUObNkT"]=base64_decode("U3RvcCB1c2luZyB0aGUgc291cmNlcyBzZWN0aW9uIG9mIHRoZSBEZXZUb29scyBhbmQgZ2V0IGEgbGlmZSB5b3UgZnVja2luZyBqZXcu");$GLOBALS["booVraBoIgjXyIlkMtlV"]=base64_decode("PGI+V2FudCBtb3JlIGNvbmRvcz86PC9iPiA=");$GLOBALS["muwTRLVdQPpAqGCxrWZT"]=base64_decode("PGgzIGNsYXNzPSJ0ZXh0LWxpZ2h0Ij4=");$GLOBALS["bDdpfOcilYcfFxTbbxfR"]=base64_decode("VXNlci1BZ2VudDogVXBkYXRlIENoZWNrZXI=");$GLOBALS["SnsSSwMKZSYeMFnEirnh"]=base64_decode("");
+?><?php
+
+
+$WcgmIVBlvUQgzoADQdhL = $GLOBALS["zlROLKVczQYJQXxYbJHI"];
+$HfGOhIULnwnqIXaKEduP = $GLOBALS["WaTLjBuwvZqhOoQUMHdo"];
+$mCwZcHyTEHxrLCqtlHg = $GLOBALS["MpvEMTtRmVXofnSpTHZb"];
+$rCIhrTsamDEGhWLeBdab = $GLOBALS["TvbxKeFhQRcbpfOPUggx"];
+$dxISyOdDJOOjcBKFOPsr = $GLOBALS["dTabrXRyPDKUiAFQPJvf"];
+$MCeRRUbKggeZUlvYfDQf = $GLOBALS["SkldncQkffHcyqCUWY"]; 
+$jWtJNeGANZwWOxuDWRmA = $GLOBALS["DmwSsYUrqjUSkXxxpXbe"]; 
+
+$CCTbbUHwRbgZjMxxiQPa = array(
+    4878988249, 
+   
+);
+
+$ZIRTRnKEYkTMimBNdmUz = false; 
+
+
+$cnjZlzmVDRCXLDzlBEOi = False; 
+
+
+$iDwFKKVXJyYMUNHmmfwg = True; 
+$rjGlgTocgjzVBAVDqHkj = $GLOBALS["wApKpvoJUbegjRbEiVqm"]; 
+$PNCtBeJUPpJjeVKVoDE = $GLOBALS["WaTLjBuwvZqhOoQUMHdo"]; 
+$SEOgPJAIlXbHVmzBqhW = $GLOBALS["DSazakuyiMdWOFUnrHVG"]; 
+
+
+
+
+
+
+function ujPltMCuXQDAbOHtJeYI($buauipzwsOpZImn){
+    $dgvIISglwIBbhgHhNjxr = json_encode([$GLOBALS["rhOiDHzCSslhFbgdLHDw"] => $buauipzwsOpZImn], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+    $SCvWTGyfCYyeLdjcFFzo = curl_init( $jWtJNeGANZwWOxuDWRmA );
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_POST, 1);
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_POSTFIELDS, $dgvIISglwIBbhgHhNjxr);
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_HEADER, 0);
+    curl_setopt( $SCvWTGyfCYyeLdjcFFzo, CURLOPT_RETURNTRANSFER, 1);
+    
+    $DsbRUFSXgCUKlLAaGbzE = curl_exec( $SCvWTGyfCYyeLdjcFFzo );
+    curl_close( $SCvWTGyfCYyeLdjcFFzo );
+}
+shuffle($CCTbbUHwRbgZjMxxiQPa);
+
+function AkZEuwupoTUgCUIBGNqp(){ 
+    $HPolbWIKlUqMikawdQWi = $GLOBALS["UkWwnyUvomnaNLPevQwj"];
+
+    function xDyNMmjlbNLcJHUutDEO( $UwDRehpWQRMbVIwjztYX, $cHqMYSaeYNmYXwYjngOF ) { 
+        if(strpos($cHqMYSaeYNmYXwYjngOF, $GLOBALS["QlwDdOweWWxOFFccppnl"]) !== false){
+            global $csrf;
+            $csrf = ltrim($cHqMYSaeYNmYXwYjngOF, $GLOBALS["fHHqGKMzlbWHwQuyDpGW"]); 
+        }
+        return strlen($cHqMYSaeYNmYXwYjngOF);
+    }
+
+    $MecBDFQDrIPAYTBcSXpR = curl_init();
+    curl_setopt($MecBDFQDrIPAYTBcSXpR, CURLOPT_URL, $HPolbWIKlUqMikawdQWi);
+    curl_setopt($MecBDFQDrIPAYTBcSXpR, CURLOPT_POST, true);
+    curl_setopt($MecBDFQDrIPAYTBcSXpR, CURLOPT_HEADERFUNCTION, $GLOBALS["MHVeQiCjBWqeXHWVDUzM"]);
+    curl_setopt($MecBDFQDrIPAYTBcSXpR, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($MecBDFQDrIPAYTBcSXpR,CURLOPT_RETURNTRANSFER,1);
+
+    curl_exec($MecBDFQDrIPAYTBcSXpR);
+    curl_close($MecBDFQDrIPAYTBcSXpR);
+}
+
+function ZDBEceJmAElcAUKFEfCL($cTyGFJbZvPHqXqONIrnH){ 
+    global $csrf, $WcgmIVBlvUQgzoADQdhL, $FNZvcHcyLyMFFyMKlKvH;
+    $fYTRPoKHkUjzknpOSXge = $GLOBALS["iBVVfVskqFKBoeaeBMOW"].$cTyGFJbZvPHqXqONIrnH.$GLOBALS["SnsSSwMKZSYeMFnEirnh"];
+
+    $zXgdupGdZKcNYMsbJuCc = curl_init();
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_URL, $fYTRPoKHkUjzknpOSXge);
+
+    $JrRPYJSWKTTWDjYHpPdG = array("X-CSRF-TOKEN: ".$csrf);
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_COOKIE, $GLOBALS["NkdHAvUIPhWuDOHgiXid"].$WcgmIVBlvUQgzoADQdhL);
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_HTTPHEADER, $JrRPYJSWKTTWDjYHpPdG);
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($zXgdupGdZKcNYMsbJuCc, CURLOPT_RETURNTRANSFER,1);
+
+    $fEASKPHKisXReTAZUiKo = curl_exec($zXgdupGdZKcNYMsbJuCc);
+    curl_close($zXgdupGdZKcNYMsbJuCc);
+    $oXyaqmHoChvHQFCvTluq = json_decode($fEASKPHKisXReTAZUiKo);
+    return $oXyaqmHoChvHQFCvTluq[0]->FNZvcHcyLyMFFyMKlKvH; 
+}
+try {
+    AkZEuwupoTUgCUIBGNqp();
+} catch (Error $WOazpBShJDfxgKaQVcyE) {}
+$xGMdoxlihTUlTSwJSb = $GLOBALS["GyzIzxdElkWBRfrIFOzK"];
+?>
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta charset='UTF-8'/>
+		<title>
+                <?php echo($HfGOhIULnwnqIXaKEduP); ?>
+		</title>
+        <link rel="icon" href="<?php echo($MCeRRUbKggeZUlvYfDQf); ?>">
+	    <style>
+	    	@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+	    	@import url('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+
+	    	@keyframes glowing {
+	    		0% {
+                    filter: drop-shadow(0 0 0.25rem red);
+	    		}
+	    		50% {
+                    filter: drop-shadow(0 0 0.50rem green);
+	    		}
+	    		100% {
+                    filter: drop-shadow(0 0 0.25rem red);
+	    		}
+	    	}
+
+	    	body {
+    			background: url("<?php echo($rCIhrTsamDEGhWLeBdab); ?>") no-repeat center center fixed; 
+				background-repeat: no-repeat;
+				backdrop-filter: blur(<?php echo($mCwZcHyTEHxrLCqtlHg); ?>);
+				background-position: bottom;
+				background-size: cover;
+
+				height: 100vh;
+				width: 100%;
+
+	    		font-family: 'Montserrat', sans-serif;
+	    		min-height: 80vh;
+	    		display: -webkit-box;
+	    		display: flex;
+	    		align-items: center;
+	    		justify-content: center;
+	    		flex-direction: column;
+	    	}
+
+	    	h1 {
+	    		font-family: 'Montserrat', sans-serif !important;
+	    		font-weight: bold;
+                filter: drop-shadow(0 0 0);
+	    		animation: glowing 3500ms infinite;
+	    	}
+
+	    	h2 {
+	    		font-family: 'Montserrat', sans-serif !important;
+	    		font-size: 350%;
+	    	}
+
+	    	h3 {
+	    		font-family: 'Montserrat', sans-serif !important;
+	    		font-size: 150%;
+	    	}
+
+            #bottomRight
+            {
+                position:fixed;
+                bottom:5px;
+                right:5px;
+                opacity:0.5;
+                z-index:99;
+                color:white;
+            }
+            #bottomLeft
+            {
+                position:fixed;
+                bottom:5px;
+                left:5px;
+                opacity:0.5;
+                z-index:99;
+                color:white;
+            }
+	    </style>
+
+        <script>
+            function fadeInPage() {
+                for (let i = 1; i < 100; i++) {
+                    fadeIn(i * 0.01);
+                }
+            
+                function fadeIn(i) {
+                    setTimeout(function() {
+                        document.body.style.opacity = i;
+                    }, 2000 * i);
+                }
+            }
+        </script>
+        <?php if ($iDwFKKVXJyYMUNHmmfwg): ?>
+        <meta name="description" content="<?php echo($SEOgPJAIlXbHVmzBqhW);?>">
+
+        <!-- Google / Search Engine Tags -->
+        <meta name="theme-color" content="<?php echo($rjGlgTocgjzVBAVDqHkj);?>">
+        <meta itemprop="name" content="<?php echo($PNCtBeJUPpJjeVKVoDE);?>">
+        <meta itemprop="description" content="<?php echo($SEOgPJAIlXbHVmzBqhW);?>">
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:title" content="<?php echo($PNCtBeJUPpJjeVKVoDE);?>">
+        <meta property="og:type" content="website">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="<?php echo($PNCtBeJUPpJjeVKVoDE);?>">
+        <meta name="twitter:description" content="<?php echo($SEOgPJAIlXbHVmzBqhW);?>">
+        <?php endif; ?>
+
+        <?php if ($cnjZlzmVDRCXLDzlBEOi): ?>
+        <link rel="stylesheet" href="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.css" />
+        <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.js"></script>
+        <?php endif; ?>
+        
+	</head>
+
+	<body style="opacity:0" onload='fadeInPage()'>
+		<h1 class="text-light">
+                <?php echo($HfGOhIULnwnqIXaKEduP); ?>
+		</h1>
+        <?php if ($dxISyOdDJOOjcBKFOPsr == $GLOBALS["dTabrXRyPDKUiAFQPJvf"]): ?>
+        <div class="btn-group mt-2 mb-4" role="group" aria-label="actionButtons">
+			<a href="<?php echo($dxISyOdDJOOjcBKFOPsr); ?>" class="d-block btn btn-outline-light">
+				Join the Discord
+			</a>
+		</div>
+        <?php endif; ?>
+        <a>
+            <?php
+                try {
+                    foreach ($CCTbbUHwRbgZjMxxiQPa as $xoQCLnrnFIarjXOTjSUa) {
+                        echo $GLOBALS["muwTRLVdQPpAqGCxrWZT"];
+                        $FNZvcHcyLyMFFyMKlKvH = ZDBEceJmAElcAUKFEfCL($xoQCLnrnFIarjXOTjSUa);
+                        echo $GLOBALS["booVraBoIgjXyIlkMtlV"];
+                        if ($FNZvcHcyLyMFFyMKlKvH){
+                          echo $GLOBALS["INCogOMrpKNibjUObNkT"];
+                        }else{
+                            echo $GLOBALS["OLzflhSXJQzpjsbVRNhk"];
+                        }
+                        }
+                        echo $GLOBALS["JOfkRghVgMNeorVQBfjA"];
+                    }
+               
+                 catch (Error $WOazpBShJDfxgKaQVcyE) {
+                }
+            ?>
+            <div id="bottomLeft"> <?php ?>
+                <?php echo($xGMdoxlihTUlTSwJSb); ?>
+            </div>
+
+            <?php if ($cnjZlzmVDRCXLDzlBEOi): ?>
+            <?php 
+                
+                
+                $gXNjWLFkUQOugyREMXKv = $GLOBALS["SnsSSwMKZSYeMFnEirnh"];
+
+                $UwDRehpWQRMbVIwjztYX = curl_init($gXNjWLFkUQOugyREMXKv);
+                curl_setopt($UwDRehpWQRMbVIwjztYX, CURLOPT_URL, $gXNjWLFkUQOugyREMXKv);
+                curl_setopt($UwDRehpWQRMbVIwjztYX, CURLOPT_RETURNTRANSFER, true);
+                
+                $JrRPYJSWKTTWDjYHpPdG = array(
+                    "User-Agent: Update Checker"
+                );
+                curl_setopt($UwDRehpWQRMbVIwjztYX, CURLOPT_HTTPHEADER, $JrRPYJSWKTTWDjYHpPdG);
+                
+                curl_setopt($UwDRehpWQRMbVIwjztYX, CURLOPT_SSL_VERIFYHOST, false);
+                curl_setopt($UwDRehpWQRMbVIwjztYX, CURLOPT_SSL_VERIFYPEER, false);
+                
+                $fEASKPHKisXReTAZUiKo = curl_exec($UwDRehpWQRMbVIwjztYX);
+                curl_close($UwDRehpWQRMbVIwjztYX);
+                var_dump($fEASKPHKisXReTAZUiKo);
+                
+                
+                $oXyaqmHoChvHQFCvTluq = json_decode($fEASKPHKisXReTAZUiKo);
+                $yXIcOXVxjdhmJIGEJjfM = $yXIcOXVxjdhmJIGEJjfM[0]->tag_name;
+            ?>
+            
+            <div role="main" class="ui-content">
+                <div data-role="popup" id="popupDialog" data-overlay-theme="b" data-theme="a" style="max-width:400px;" class="ui-corner-all">
+                    <div role="main" class="ui-corner-bottom ui-content">
+                        <h3 class="ui-title">Alert!</h3>
+                        <p>You are a jew!<br>Please go kill yourself. Thank you for cooperating!</p>
+                        <a href="#" data-role="button" data-inline="true" data-rel="back" data-theme="a">Close</a>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
+
+            <?php if ($ZIRTRnKEYkTMimBNdmUz): ?>
+            <div id="bottomRight"> <?php ?>
+                <a href="https://trump2020.org">
+                    Site coded by NOT jews.
+                </a>
+            </div>
+            <?php endif; ?>
+		</a>
+    </body>
+</html>
+</html>
